7fd1ebbcb51ee38a60abdd1f1cd95afaf970334c
Adam Jacobs <ajman0390@gmail.com>
2019-07-26 16:55:02 -0400

    Updates to .html pages and .js files

--- js/getCompound.js
+++ js/getCompound.js
@@ -5,30 +5,29 @@ var timesCompounded = 0;
 var termOfLoan = 0;
 var amount = 0;
 
-function simpleInterest() {
-  event.preventDefault();
-  var principal = parseFloat(document.getElementById("principalSimple").value);
-  var interestRate = parseFloat(document.getElementById("interestSimple").value);
-  interestRate = interestRate / 100;
-  var termOfLoan = parseFloat(document.getElementById("termSimple").value);
-  var simpleInt = principal * interestRate * termOfLoan;
-  var amount = (principal + simpleInt).toFixed(2);
-  document.getElementById("siOutput-01").innerHTML = "Interest: $" + simpleInt.toFixed(2);
-  document.getElementById("siOutput-02").innerHTML = "Total plus interest: $" + amount;
+window.onload = function()
+{
+  document.getElementById("btnCompound").onclick = calculate;
 }
 
-function compoundInterest() {
-  event.preventDefault();
-  var principal = parseFloat(document.getElementById("principalCompound").value);
-  var interestRate = parseFloat(document.getElementById("interestCompound").value);
-  interestRate = interestRate / 100;
-  var timesCompounded = parseFloat(document.getElementById("timesCompounded").value);
-  var termOfLoan = parseFloat(document.getElementById("termCompound").value);
-  var a = interestRate / timesCompounded;
-  var b = 1 + a;
-  var c = timesCompounded * termOfLoan;
-  var d = Math.pow(b, c);
-  var amount = (principal * d).toFixed(2);
-  document.getElementById("ciOutput-01").innerHTML = "Interest: $" + (amount - principal).toFixed(2);
-  document.getElementById("ciOutput-02").innerHTML = "Total plus interest: $" + amount;
-}
+function calculate()
+            {
+                var p = document.getElementById("principalCompound").value;
+                
+                var t = document.getElementById("termCompound").value; // no. of years
+                var r = document.getElementById("interestCompound").value;
+                // result = document.getElementById("result");
+
+                // The equation is A = p * [[1 + (r/n)] ^ nt]
+                var A = (p* Math.pow((1 + (r/(1*100))), (1*t)));
+
+                // toFixed is used for rounding the amount with two decimal places.
+
+                console.log(A);
+                
+                
+
+                document.getElementById("ciOutput-01").value = A.toFixed(2);
+
+                document.getElementById("ciOutput-02").value =  (A.toFixed(2) - p).toFixed(2);
+            }
